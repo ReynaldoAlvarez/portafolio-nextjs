55658de1ed54f8d31ac9f9070dfae8fe12777fd1
Vidal Alvarez <vidal.r.a.777@gmail.com>
2023-07-09 10:07:04 -0400

    add analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import { Montserrat } from 'next/font/google'
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Montserrat({ subsets: ['latin'] })
 
 
@@ -20,6 +22,7 @@ export default function RootLayout({
         <body className={`${inter.className} bg-light dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 pr-16 pl-16`}>
         <NavBar />
           {children}
+          <Analytics />
           <Footer />
           </body>
     </html>
